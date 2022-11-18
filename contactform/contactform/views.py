e81771c8eb27efeba1cd04483f76266208d898a3
Rauf Alibakhshov <91628590+Alibakhshov@users.noreply.github.com>
2022-11-18 13:05:17 +0600

    Getting the data from the user

--- contactform/contactform/views.py
+++ contactform/contactform/views.py
@@ -2,5 +2,20 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
+    if request.method == 'POST':
+        name = request.POST.get('fullName')
+        email = request.POST.get('email')
+        subject = request.POST.get('subject')
+        message = request.POST.get('message')
+        
+        data = {
+            'name': name,
+            'email': email,
+            'subject': subject,
+            'message': message
+        }
+        
+        print(data)
+       ## return HttpResponse('Name: ' + name + ' Email: ' + email + ' Message: ' + message)
     return render(request, 'main.html', {})
 
