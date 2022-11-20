1895c52785808455be9ebab6063750deb0b1dd2b
Rauf Alibakhshov <91628590+Alibakhshov@users.noreply.github.com>
2022-11-20 14:16:04 +0600

    updates

--- contactform/contactform/settings.py
+++ contactform/contactform/settings.py
@@ -144,5 +144,6 @@ EMAIL_USE_SSL = False
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATICFILES_STORAGE='whitenoise.django.GzipManifestStaticFilesStorage'
 
 django_heroku.settings(locals())
