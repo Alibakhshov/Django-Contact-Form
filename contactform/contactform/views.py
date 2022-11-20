f1d0f1ec23456f8db64b6cf206ef077821d03f04
Rauf Alibakhshov <91628590+Alibakhshov@users.noreply.github.com>
2022-11-20 11:24:16 +0600

    updates

--- contactform/contactform/views.py
+++ contactform/contactform/views.py
@@ -30,16 +30,16 @@ def index(request):
         
         Email of the Intern: {}
         
-        Checked place: {}
+        Classroom(TV Lounge): {}
         
-        Equipments in 
+        Classroom(TV Lounges) equipments: {}
         
-        Missing Equipments: {}
+        Missing equipments: {}
         
         Other comments: {}
         
         
-        '''.format( data['name'],  data['email'], data['classrooms'] , data['message'])
+        '''.format( data['name'],  data['email'], data['classrooms'], data['equipment'], data['missEquipment'], data['message'])
         
         send_mail(data['subject'], message, '', ['raufalibakhshov4@gmail.com'], ['raufalibakhshov02@gmail.com'])
         # send_mail(
